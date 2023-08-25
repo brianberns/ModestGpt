58209eddac5160c31f2bccd82a6419e1780779b9
Brian Berns <brianberns@gmail.com>
2023-08-24 23:31:30 -0400

    Char GPT.

--- ModestGpt/Trainer/Trainer.fs
+++ ModestGpt/Trainer/Trainer.fs
@@ -95,7 +95,7 @@ module Trainer =
 
                     let tnow = DateTime.Now
                     let iter_dt = tnow - iterTime
-                    if iterNum % 100 = 0 then
+                    if iterNum % 10 = 0 then
                         printfn $"iter {iterNum}: loss {loss.item<float32>()}"
                     tnow
 
@@ -108,99 +108,77 @@ module Trainer =
 
         train_loader.GetEnumerator() |> loop 0 DateTime.Now
 
-/// Dataset for the Sort problem. E.g. for problem length 6:
-/// Input: 0 0 2 1 0 1 -> Output: 0 0 0 1 1 2
-/// Which will feed into the transformer concatenated as:
-/// input:  0 0 2 1 0 1 0 0 0 1 1
-/// output: I I I I I 0 0 0 1 1 2
-/// where I is "ignore", as the transformer is reading the input sequence
-type SortDataset(split, ?length, ?num_digits) =
+type CharDatasetConfig =
+    {
+        block_size : int
+    }
+
+/// Emits batches of characters
+type CharDataset(config, data : string) =
     inherit Dataset()
 
-    let length = defaultArg length 6
-    let num_digits = defaultArg num_digits 3
+    let chars = set data
+    let data_size, vocab_size_ = data.Length, chars.Count
+    do printfn "data has %d characters, %d unique." data_size vocab_size_
 
-    do assert(List.contains split ["train"; "test"])
+    let stoi = Map [ for i, ch in Seq.indexed chars -> ch, i ]
+    let itos = Map [ for i, ch in Seq.indexed chars -> i, ch ]
 
-    let nTensorPairs = 10000
+    static member get_default_config() =
+        {
+            block_size = 128
+        }
 
-    let makeTensorPair _ =
+    member _.Itos(i) = itos[i]
+    member _.Stoi(ch) = stoi[ch]
 
-        // use rejection sampling to generate an input example from the desired split
-        let rec loop () =
-            // generate some random integers
-            let inp = torch.randint(int64 num_digits, size=[|int64 length|], dtype=torch.long)
-            // half of the time let's try to boost the number of examples that 
-            // have a large number of repeats, as this is what the model seems to struggle
-            // with later in training, and they are kind of rare
-            let reject =
-                if torch.rand(1).item() < 0.5f then
-                    let struct (unique, _, _) = inp.unique()
-                    // too many unqiue digits, re-sample
-                    unique.NumberOfElements > int64 length / 2L
-                else false
-            if reject then loop ()
-            else
-                // figure out if this generated example is train or test based on its hash
-                let inp_split = if torch.rand(1).item() < 0.25f then "test" else "train" // designate 25% of examples as test
-                if inp_split = split then
-                    inp
-                else loop ()
+    member _.get_vocab_size() =
+        vocab_size_
 
-        let inp = loop ()
+    member _.get_block_size() =
+        config.block_size
 
-        /// First item of a value tuple.
-        let fstv (struct (x, _)) = x
-
-        /// Second item of a value tuple.
-        let sndv (struct (_, y)) = y
-        
-        // solve the task: i.e. sort
-        let sol = torch.sort(inp) |> fstv
-
-        // concatenate the problem specification and the solution
-        let cat = torch.cat([|inp; sol|], dim=0)
-
-        // the inputs to the transformer will be the offset sequence
-        let x = cat[Slice(stop = -1)].clone()
-        let y = cat[Slice(1)].clone()
-        // we only want to predict at output locations, mask out the loss at the input locations
-        y[Slice(stop=length-1)] <- tensor -1
-        x, y
-
-    let tensorPairs =
-        Array.init nTensorPairs makeTensorPair
-
-    member _.Length = length
-
-    override _.Count with get() = nTensorPairs
-
-    member _.get_vocab_size() = num_digits
-
-    member _.get_block_size() = length * 2 - 1
+    override _.Count with get() =
+        int64 (data.Length - config.block_size)
 
     override _.GetTensor(idx) =
-        tensorPairs[int idx]
+        // grab a chunk of (block_size + 1) characters from the data
+        let chunk = data[int idx .. int idx + config.block_size]
+        assert(chunk.Length = config.block_size + 1)
+        // encode every character to an integer
+        let dix = [| for ch in chunk -> stoi[ch] |]
+        // return as tensors
+        let x = torch.tensor(dix[.. dix.Length-2], dtype=torch.long)
+        let y = torch.tensor(dix[1 ..], dtype=torch.long)
+        x, y
 
 module Program =
 
     [<EntryPoint>]
     let main args =
+
         setSeed 0
+
+        // construct the training dataset
+        let dataset =
+            let text = System.IO.File.ReadAllText(@"Trainer\Input.txt")
+            new CharDataset({ block_size = 128 }, text)
+
         use model =
             new Gpt {
-                NumLayer = 3
-                NumHead = 3
-                NumEmbed = 48
-                VocabSize = 3
-                BlockSize = 6 * 2 - 1
+                NumLayer = 6
+                NumHead = 6
+                NumEmbed = 192
+                VocabSize = dataset.get_vocab_size()
+                BlockSize = dataset.get_block_size()
                 Dropout = 0.1
             }
+
         let config =
             {
                 Device = "auto"
-                NumWorkers = 0
-                MaxIters = 2000
+                NumWorkers = 4
+                MaxIters = -1
                 BatchSize = 64
                 LearningRate = 5e-4
                 Beta1 = 0.9
@@ -208,7 +186,7 @@ module Program =
                 WeightDecay = 0.1 // only applied on matmul weights
                 GradNormClip = 1.0
             }
-        use dataset = new SortDataset("train")
+
         Trainer.run config model dataset
                 
         0
